c72c0a23a83a2c48d80052d17081012eea31d2dd
sakis5668 <sakis.tosounidis@gmail.com>
2021-12-01 15:13:43 +0200

    database connection

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from Ui_main import Ui_MainWindow
 
+from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
+
+    connection = QSqlDatabase.addDatabase("QSQLITE")
+    connection.setDatabaseName("test.db")
+
+    app = QApplication(sys.argv)
+
+    if not connection.open():
+        QMessageBox.critical(
+            None,
+            "Error !",
+            "Database error: %s" % connection.lastError().databaseText(),
+        )
+        sys.exit(1)
+
+
+    MainWindow = QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
